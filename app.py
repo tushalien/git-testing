0407b4b525c5307cfd3840dfd5e2c0bae6a1c01b
Tushar Agrawal <agrawaltushar63@gmail.com>
2020-05-22 21:37:30 +0530

    Added test file

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask 
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return "Hello World!!"
+
+@app.route('/tushar')
+def tushar():
+	return "Hello Tushar!!"
+
+# Yash Route - to be writtern by yash
+
+app.run(host='0.0.0.0', port =8000)
